4146b830a910476bbc02bcae671265cf41bf38bd
Nelio Frazao <neliofrazac@gmail.com>
2025-02-08 19:26:21 -0300

    test: display error message when submitting form with invalid email format

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,10 +1,10 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
+  const longText = Cypress._.repeat('abcdefghij', 10)
   beforeEach(() => cy.visit('./src/index.html'))
   it('verifica o título da aplicação', () => {
     cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
   })
   it('preenche os campos obrigatórios e envia o formulário.', ()=> {
-    const longText = Cypress._.repeat('abcdefghij', 10)
     cy.get('#firstName').type('Fulano')
     cy.get('#lastName').type('de Tal')
     cy.get('#email').type('fulando@email.com')
@@ -14,6 +14,11 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
 
   it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
-
+    cy.get('#firstName').type('Fulano')
+    cy.get('#lastName').type('de Tal')
+    cy.get('#email').type('fulando@email,com')
+    cy.get('#open-text-area').type(longText, {delay: 0})
+    cy.get('button[type="submit"]').click()
+    cy.get('.error').should('be.visible')
   })
 })
